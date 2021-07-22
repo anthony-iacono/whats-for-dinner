d551c0281ed57ee1a8c824ee7f6ee0b0fb630c7b
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-07-22 13:13:28 -0700

    Add letsCookButton event listener

--- main.js
+++ main.js
@@ -2,17 +2,42 @@
 
 // [x] create random index helper function
 
-// [] assign variables storing random index from each array
+// [x] assign variables storing random index from each array
 
-// store let's cook button with query Selector
+// [x] store let's cook button with query Selector
 
-// store output div with querySelector
+// [x] store output div with querySelector
 
-// add eventListener to let's cook button
+// [x] add eventListener to let's cook button
 
-// add event handler that clears output divs html and injects a template literal with random dish from selected array using random index
+// [] add event handler that clears output divs html and injects a template literal with random dish from selected array using random index
 
+// Variables
 
+let randomSidesIndex = getRandomIndex(sides);
+let randomMainsIndex = getRandomIndex(mains);
+let randomDessertsIndex = getRandomIndex(desserts);
+
+// HTML Elements
+
+const letsCookButton = document.querySelector('.js-lets-cook-button');
+const output = document.querySelector('.js-output-box');
+
+// Event Listeners
+
+letsCookButton.addEventListener('click', displayRandomDish);
+
+// Event Handlers
+
+function displayRandomDish() {
+  output.innerHTML = '';
+  dishType = letsCookButton.value;
+  if (dishType === 'side') {
+    output.innHTML = `
+    <p>You should make:\n${sides[randomSidesIndex]}</p>
+    `
+  }
+}
 
 // Helper Functions
 
