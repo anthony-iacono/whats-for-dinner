1a650f8ff7d3fe10f6db5b5ee4077988edd070cd
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-07-22 12:55:36 -0700

    Add random index helper function

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // When a user selects a dish option (don’t worry about “Entire Meal”) and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
 
-// store radio buttons in variables with querySelector
-//
+// [x] create random index helper function
+
+// [] assign variables storing random index from each array
+
+// store let's cook button with query Selector
+
+// store output div with querySelector
+
+// add eventListener to let's cook button
+
+// add event handler that clears output divs html and injects a template literal with random dish from selected array using random index
 
 
-let sides = explode('\n', `Miso Glazed Carrots
-Coleslaw
-Garden Salad
-Crispy Potatoes
-Sweet Potato Tots
-Coconut Rice
-Caeser Salad
-Shrimp Summer Rolls
-Garlic Butter Mushrooms
-Hush Puppies`);
 
-console.log(sides);
+// Helper Functions
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * (array.length + 1))
+}
