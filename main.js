30f4a8909234d571727044b2af0fb8a95a66438e
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-07-22 12:40:11 -0700

    Add data.js with dish arrays

---
+++ main.js
@@ -0,0 +1,18 @@
+// When a user selects a dish option (don’t worry about “Entire Meal”) and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
+
+// store radio buttons in variables with querySelector
+//
+
+
+let sides = explode('\n', `Miso Glazed Carrots
+Coleslaw
+Garden Salad
+Crispy Potatoes
+Sweet Potato Tots
+Coconut Rice
+Caeser Salad
+Shrimp Summer Rolls
+Garlic Butter Mushrooms
+Hush Puppies`);
+
+console.log(sides);
